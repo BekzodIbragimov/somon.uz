512d392c622261ef81a0092bec25f7efbe72eeee
Shahzod Jabbarov <jabborovshaxzod2002@gmail.com>
2022-07-20 12:46:01 +0500

    aaaa

--- home/models.py
+++ home/models.py
@@ -1,2 +1,17 @@
 from django.db import models
 
+
+class House(models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pics')
+    desc = models.TextField()
+    price = models.IntegerField()
+    viloyat = models.CharField(max_length=100)
+    tuman = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    number = models.CharField(max_length=15)
+    qurilgan = models.IntegerField()
+    
+    
+
+>>>>>>> 1a67676f1c714e28035cf2145e493073804a6609
