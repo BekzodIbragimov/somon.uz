1a67676f1c714e28035cf2145e493073804a6609
BekzodIbragimov <97014290+BekzodIbragimov@users.noreply.github.com>
2022-07-20 12:41:46 +0500

    model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class House(models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pics')
+    desc = models.TextField()
+    price = models.IntegerField()
+    viloyat = models.CharField(max_length=100)
+    tuman = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    number = models.CharField(max_length=15)
+    qurilgan = models.IntegerField()
+    
+    
+
