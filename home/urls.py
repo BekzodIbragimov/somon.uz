a0782d4d0307424436973249aeb6b390b475b4e0
BekzodIbragimov <97014290+BekzodIbragimov@users.noreply.github.com>
2022-07-25 13:04:10 +0500

    part search

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('search_house', views.search_house, name='search_house'),
     
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from .models import House
+
+
 def home(request):
     houses = House.objects.all()
     return render(request, 'home.html', {'houses': houses})
     
+def search_house(request):
+    
+    if request.method == 'POST':
+    
+        searched = request.POST['searched']
+        return render(request, 'search.html', {'searched':searched})
+    else:
+        return render(request, 'search.html')
