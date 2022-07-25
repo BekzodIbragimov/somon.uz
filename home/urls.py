51d996b6888ff7913fb29fba53926bbd69f3be28
BekzodIbragimov <97014290+BekzodIbragimov@users.noreply.github.com>
2022-07-25 13:09:08 +0500

    bashara

--- home/urls.py
+++ home/urls.py
@@ -3,11 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-<<<<<<< HEAD
     path('search_house', views.search_house, name='search_house'),
-    
-=======
     path('home_detail/<detail_id>', views.post, name='post-detail'),
->>>>>>> 1d4d21a8b8a50eebdb0b5b3d659410afd9d68b40
 ]
     
