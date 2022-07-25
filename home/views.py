45145e68dd9841127a045b377aeb3ad3ed03358b
BekzodIbragimov <97014290+BekzodIbragimov@users.noreply.github.com>
2022-07-25 13:05:56 +0500

    hey

--- home/views.py
+++ home/views.py
@@ -6,7 +6,7 @@ def home(request):
     houses = House.objects.all()
     return render(request, 'home.html', {'houses': houses})
     
-<<<<<<< HEAD
+
 def search_house(request):
     
     if request.method == 'POST':
@@ -15,9 +15,8 @@ def search_house(request):
         return render(request, 'search.html', {'searched':searched})
     else:
         return render(request, 'search.html')
-=======
+
 def post(request, detail_id):
     postid = House.objects.get(pk=detail_id)
     return render(request, 'post_detail.html', 
     {"postid":postid}) 
->>>>>>> 1d4d21a8b8a50eebdb0b5b3d659410afd9d68b40
